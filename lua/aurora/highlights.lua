5acced75cbcd9ca95137cc6fb5aab180389b1f06
christianchiarulli <chrisatmachine@gmail.com>
2021-07-11 22:32:17 -0400

    update

--- lua/aurora/highlights.lua
+++ lua/aurora/highlights.lua
@@ -23,7 +23,7 @@ local highlights = {
 		CursorLine = {bg = C.alt_bg, },
 		CursorColumn = {bg = C.alt_bg, },
 		ColorColumn = {bg = C.alt_bg, },
-		NormalFloat = {bg = C.bg, },
+		NormalFloat = {bg = C.alt_bg, },
 		Visual = {bg = C.alt_bg, },
 		VisualNOS = {bg = C.alt_bg, },
 		WarningMsg = {fg = C.error_red, },
