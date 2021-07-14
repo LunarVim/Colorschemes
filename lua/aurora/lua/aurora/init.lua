46159f2fbaad61977f7c20f04c5ac9a7706d4650
Murtaza Udaipurwal <murtaza@machine>
2021-07-14 20:09:26 +0530

    Added support for asynchonous loading of colorscheme

--- lua/aurora/lua/aurora/init.lua
+++ lua/aurora/lua/aurora/init.lua
@@ -9,18 +9,36 @@ vim.g.colors_name = "aurora"
 local util = require("aurora.util")
 Config = require("aurora.config")
 C = require("aurora.palette")
+
+local async
+async = vim.loop.new_async(vim.schedule_wrap(function ()
+    local markdown = require("aurora.markdown")
+	local Whichkey = require("aurora.Whichkey")
+	local Git = require("aurora.Git")
+	local LSP = require("aurora.LSP")
+	
+
+    local skeletons = {
+        markdown, Whichkey, Git, LSP
+    }
+
+    for _, skeleton in ipairs(skeletons) do
+        util.initialise(skeleton)
+    end
+
+    async:close()
+end))
+
 local highlights = require("aurora.highlights")
 local Treesitter = require("aurora.Treesitter")
-local markdown = require("aurora.markdown")
-local Whichkey = require("aurora.Whichkey")
-local Git = require("aurora.Git")
-local LSP = require("aurora.LSP")
 
 
 local skeletons = {
-    highlights, Treesitter, markdown, Whichkey, Git, LSP
+    highlights, Treesitter
 }
 
 for _, skeleton in ipairs(skeletons) do
     util.initialise(skeleton)
 end
+
+async:send()
