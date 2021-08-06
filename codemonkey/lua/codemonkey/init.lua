6ec1ee40bbc54b967dbc44d4f61dd6fdc0caf8cd
christianchiarulli <chrisatmachine@gmail.com>
2021-08-06 17:15:40 -0400

    update

---
+++ codemonkey/lua/codemonkey/init.lua
@@ -0,0 +1,44 @@
+vim.api.nvim_command("hi clear")
+if vim.fn.exists("syntax_on") then
+    vim.api.nvim_command("syntax reset")
+end
+vim.o.background = "dark"
+vim.o.termguicolors = true
+vim.g.colors_name = "codemonkey"
+
+local util = require("codemonkey.util")
+Config = require("codemonkey.config")
+C = require("codemonkey.palette")
+
+local async
+async = vim.loop.new_async(vim.schedule_wrap(function ()
+    
+
+    local skeletons = {
+        
+    }
+
+    for _, skeleton in ipairs(skeletons) do
+        util.initialise(skeleton)
+    end
+
+    async:close()
+end))
+
+local highlights = require("codemonkey.highlights")
+local Treesitter = require("codemonkey.Treesitter")
+local markdown = require("codemonkey.markdown")
+local Whichkey = require("codemonkey.Whichkey")
+local Git = require("codemonkey.Git")
+local LSP = require("codemonkey.LSP")
+
+
+local skeletons = {
+    highlights, Treesitter, markdown, Whichkey, Git, LSP
+}
+
+for _, skeleton in ipairs(skeletons) do
+    util.initialise(skeleton)
+end
+
+async:send()
