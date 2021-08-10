a3a315b087ef03270c8719dafb2817e3190679f7
Chris <chris@macbook.local>
2021-08-10 01:17:42 -0400

    test

--- lua/system76/Treesitter.lua
+++ lua/system76/Treesitter.lua
@@ -1,5 +1,5 @@
 local Treesitter = {
-		TSComment = {fg = C.green, },
+		TSComment = {fg = C.gray, },
 		TSAnnotation = {fg = C.yellow, },
 		TSAttribute = {fg = C.cyan, },
 		TSConstructor = {fg = C.cyan, },
