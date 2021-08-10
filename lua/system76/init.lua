bef024667e23b7515221e8cffaa95999af6cc8c2
Chris <chris@macbook.local>
2021-08-10 01:13:09 -0400

    test

---
+++ lua/system76/init.lua
@@ -0,0 +1,44 @@
+vim.api.nvim_command("hi clear")
+if vim.fn.exists("syntax_on") then
+    vim.api.nvim_command("syntax reset")
+end
+vim.o.background = "dark"
+vim.o.termguicolors = true
+vim.g.colors_name = "system76"
+
+local util = require("system76.util")
+Config = require("system76.config")
+C = require("system76.palette")
+
+local async
+async = vim.loop.new_async(vim.schedule_wrap(function ()
+    
+
+    local skeletons = {
+        
+    }
+
+    for _, skeleton in ipairs(skeletons) do
+        util.initialise(skeleton)
+    end
+
+    async:close()
+end))
+
+local highlights = require("system76.highlights")
+local Treesitter = require("system76.Treesitter")
+local markdown = require("system76.markdown")
+local Whichkey = require("system76.Whichkey")
+local Git = require("system76.Git")
+local LSP = require("system76.LSP")
+
+
+local skeletons = {
+    highlights, Treesitter, markdown, Whichkey, Git, LSP
+}
+
+for _, skeleton in ipairs(skeletons) do
+    util.initialise(skeleton)
+end
+
+async:send()
