65c7381968c98a9ce0317a432b82307e34767dd5
ChristianChiarulli <chris.machine@pm.me>
2021-11-03 23:51:18 -0400

    test color

--- lua/darkplus/Cmp.lua
+++ lua/darkplus/Cmp.lua
@@ -1,7 +1,10 @@
 local Cmp = {
+		CmpItemAbbr = {fg = "#ff0000", },
+		CmpItemAbbrDeprecated = {fg = "#00ff00", },
 		CmpItemAbbrMatch = {fg = C.ui3_blue, },
 		CmpItemAbbrMatchFuzzy = {fg = C.ui3_blue, },
 		CmpItemKind = {fg = C.purple, },
+		CmpItemMenu = {fg = "#f0f00f", },
 }
 
 return Cmp
