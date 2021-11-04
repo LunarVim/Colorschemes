b586d2605b94532e483049f35e4c4475fc45e0ab
ChristianChiarulli <chris.machine@pm.me>
2021-11-03 23:53:58 -0400

    forgot to update

--- lua/darkplus/Cmp.lua
+++ lua/darkplus/Cmp.lua
@@ -1,10 +1,8 @@
 local Cmp = {
-		CmpItemAbbr = {fg = "#ff0000", },
-		CmpItemAbbrDeprecated = {fg = "#00ff00", },
+		CmpItemAbbrDeprecated = {fg = "#ui3_blue", },
 		CmpItemAbbrMatch = {fg = C.ui3_blue, },
 		CmpItemAbbrMatchFuzzy = {fg = C.ui3_blue, },
 		CmpItemKind = {fg = C.purple, },
-		CmpItemMenu = {fg = "#f0f00f", },
 }
 
 return Cmp
