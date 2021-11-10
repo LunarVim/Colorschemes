65ffde647d8acf8e3f859554f7ab802a3b82bebe
ChristianChiarulli <chris.machine@pm.me>
2021-11-10 17:38:21 -0500

    test

--- lua/darkplus/StatusLine.lua
+++ lua/darkplus/StatusLine.lua
@@ -2,7 +2,7 @@ local StatusLine = {
 		StatusLine = {fg = C.alt_bg, },
 		StatusLineNC = {fg = C.purple_test, bg = C.alt_bg, },
 		StatusLineSeparator = {fg = C.alt_bg, },
-		StatusLineTerm = {fg = C.alt_bg, },
+		StatusLineTerm = {fg = C.purple_test, bg = C.alt_bg, },
 		StatusLineTermNC = {fg = C.cyan_test, bg = C.alt_bg, },
 }
 
