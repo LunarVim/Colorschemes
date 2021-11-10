0ee279d2d70e1199dc5d6703d17c35c307b435ae
ChristianChiarulli <chris.machine@pm.me>
2021-11-09 22:07:47 -0500

    test

--- lua/darkplus/IndentBlankline.lua
+++ lua/darkplus/IndentBlankline.lua
@@ -1,6 +1,6 @@
 local IndentBlankline = {
 		IndentBlanklineContextChar = {fg = C.context, },
-		IndentBlanklineContextStart = {fg = C.context, },
+		IndentBlanklineContextStart = {style = "underline", },
 		IndentBlanklineChar = {fg = C.dark_gray, },
 		IndentBlanklineSpaceChar = {fg = C.cyan_test, },
 		IndentBlanklineSpaceCharBlankline = {fg = C.info_yellow, },
