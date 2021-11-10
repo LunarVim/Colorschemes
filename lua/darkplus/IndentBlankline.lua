c985f38a67aa552b4f1fd79b532bb5de39d301da
ChristianChiarulli <chris.machine@pm.me>
2021-11-09 22:06:10 -0500

    update indentline context color

--- lua/darkplus/IndentBlankline.lua
+++ lua/darkplus/IndentBlankline.lua
@@ -1,5 +1,6 @@
 local IndentBlankline = {
 		IndentBlanklineContextChar = {fg = C.context, },
+		IndentBlanklineContextStart = {fg = C.context, },
 		IndentBlanklineChar = {fg = C.dark_gray, },
 		IndentBlanklineSpaceChar = {fg = C.cyan_test, },
 		IndentBlanklineSpaceCharBlankline = {fg = C.info_yellow, },
