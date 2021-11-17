a763f4087cc05fcc00daa2c40dbd7ec2588852ae
ChristianChiarulli <chris.machine@pm.me>
2021-11-16 23:52:50 -0500

    update

--- lua/darkplus/StatusLine.lua
+++ lua/darkplus/StatusLine.lua
@@ -1,9 +1,9 @@
 local StatusLine = {
-		StatusLine = {fg = C.alt_bg, },
-		StatusLineNC = {fg = C.alt_bg, },
-		StatusLineSeparator = {fg = "#ff00ff", },
-		StatusLineTerm = {fg = "#ff00ff", },
-		StatusLineTermNC = {fg = "#ff00ff", },
+		StatusLine = {fg = C.alt_bg, bg = C.fg, },
+		StatusLineNC = {fg = C.alt_bg, bg = C.fg, },
+		StatusLineSeparator = {fg = C.alt_bg, },
+		StatusLineTerm = {fg = C.alt_bg, },
+		StatusLineTermNC = {fg = C.alt_bg, },
 }
 
 return StatusLine
