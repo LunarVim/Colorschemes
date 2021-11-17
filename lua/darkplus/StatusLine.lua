ed5a2f1ceaa9ec80b4c05c618b98d47baeb22ba0
ChristianChiarulli <chris.machine@pm.me>
2021-11-16 23:54:12 -0500

    fix statusline colors

--- lua/darkplus/StatusLine.lua
+++ lua/darkplus/StatusLine.lua
@@ -1,6 +1,6 @@
 local StatusLine = {
-		StatusLine = {fg = C.alt_bg, bg = C.fg, },
-		StatusLineNC = {fg = C.alt_bg, bg = C.fg, },
+		StatusLine = {fg = C.alt_bg, bg = C.gray, },
+		StatusLineNC = {fg = C.alt_bg, bg = C.gray, },
 		StatusLineSeparator = {fg = C.alt_bg, },
 		StatusLineTerm = {fg = C.alt_bg, },
 		StatusLineTermNC = {fg = C.alt_bg, },
