80698d681ff9bb967486e1fd88ab742f80ec7b53
ChristianChiarulli <chris.machine@pm.me>
2022-07-09 00:46:26 -0400

    update

--- lua/onedarker/Hop.lua
+++ lua/onedarker/Hop.lua
@@ -1,7 +1,7 @@
 local Hop = {
-		HopNextKey = {fg = "#8ae0ff", },
-		HopNextKey1 = {fg = "#d48EeD", },
-		HopNextKey2 = {fg = "#b46EcD", },
+		HopNextKey = {fg = "#6ae0ff", },
+		HopNextKey1 = {fg = "#d46EeD", },
+		HopNextKey2 = {fg = "#b44EcD", },
 		HopUnmatched = {fg = C.gray, },
 		HopPreview = {fg = "#c7bA7D", },
 }
