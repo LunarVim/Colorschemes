dd8d3b484597d802726cdbddd1ca69e377491185
ChristianChiarulli <chris.machine@pm.me>
2022-07-13 15:30:45 -0400

    update

--- lua/onedarker/Navic.lua
+++ lua/onedarker/Navic.lua
@@ -5,6 +5,17 @@ local Navic = {
 		NavicIconsPackage = {fg = C.fg, },
 		NavicIconsClass = {fg = C.cyan, },
 		NavicIconsMethod = {fg = C.blue, },
+		NavicIconsProperty = {fg = C.red, },
+		NavicIconsField = {fg = C.red, },
+		NavicIconsConstructor = {fg = C.cyan, },
+		NavicIconsEnum = {fg = C.cyan, },
+		NavicIconsInterface = {fg = C.cyan, },
+		NavicIconsFunction = {fg = C.blue, },
+		NavicIconsVariable = {fg = C.red, },
+		NavicIconsConstant = {fg = C.orange, },
+		NavicIconsString = {fg = C.green, },
+		NavicIconsNumber = {fg = C.orange, },
+		NavicIconsBoolean = {fg = C.orange, },
 }
 
 return Navic
