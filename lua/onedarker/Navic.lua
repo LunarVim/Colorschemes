95d8375d60e7ad6b8f6e19dd49352444c3624de2
ChristianChiarulli <chris.machine@pm.me>
2022-07-13 15:29:23 -0400

    update

---
+++ lua/onedarker/Navic.lua
@@ -0,0 +1,10 @@
+local Navic = {
+		NavicIconsFile = {fg = C.fg, },
+		NavicIconsModule = {fg = C.cyan, },
+		NavicIconsNamespace = {fg = C.fg, },
+		NavicIconsPackage = {fg = C.fg, },
+		NavicIconsClass = {fg = C.cyan, },
+		NavicIconsMethod = {fg = C.blue, },
+}
+
+return Navic
